7004c842b74f3d3f91f9c96cdf5b3f1cb9890fae
Abdou00 <abdoulaye_diarra@outlook.fr>
2019-10-17 12:15:53 +0200

    Add Category CRUD

--- admin/catlist.php
+++ admin/catlist.php
@@ -1,9 +1,26 @@
-﻿<?php include 'inc/header.php';?>
-<?php include 'inc/sidebar.php';?>
+﻿<?php
+    include 'inc/header.php';
+    include 'inc/sidebar.php';
+    include '../classes/Category.php';
+
+    $cat = new Category();
+    if (isset($_GET['delcat']))
+    {
+        $id = $_GET['delcat'];
+        $id = preg_replace('/[^⁻a-zA-Z0-9_]/', '', $_GET['delcat']);
+        $delCat = $cat->delCatById($id);
+    }
+?>
         <div class="grid_10">
             <div class="box round first grid">
                 <h2>Category List</h2>
-                <div class="block">        
+                <div class="block">
+                    <?php
+                    if (isset($delCat))
+                    {
+                        echo $delCat;
+                    }
+                    ?>
                     <table class="data display datatable" id="example">
 					<thead>
 						<tr>
@@ -13,46 +30,24 @@
 						</tr>
 					</thead>
 					<tbody>
+                        <?php
+                            $getCats = $cat->getAllCat();
+                            if ($getCats)
+                            {
+                                $i = 0;
+                                while ($result = $getCats->fetch_assoc())
+                                {
+                                    $i++;
+                        ?>
 						<tr class="odd gradeX">
-							<td>01</td>
-							<td>Internet</td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
+							<td><?php echo $i; ?></td>
+							<td><?php echo $result['catName']; ?></td>
+							<td><a href="catedit.php?catid=<?php echo $result['catId']; ?>">Edit</a> || <a onclick="return confirm('Are you sure to delete this category?');" href="?delcat=<?php echo $result['catId']; ?>">Delete</a></td>
 						</tr>
-						<tr class="even gradeC">
-							<td>02</td>
-							<td>Explorer </td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="odd gradeX">
-							<td>03</td>
-							<td>Internet</td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="even gradeC">
-							<td>04</td>
-							<td>Explorer </td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-							<tr class="odd gradeX">
-							<td>05</td>
-							<td>Internet</td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="even gradeC">
-							<td>06</td>
-							<td>Explorer </td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="odd gradeX">
-							<td>07</td>
-							<td>Internet</td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="even gradeC">
-							<td>08</td>
-							<td>Explorer </td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
+                        <?php
+                                }
+                            }
+                        ?>
 					</tbody>
 				</table>
                </div>
---
+++ classes/Category.php
@@ -0,0 +1,99 @@
+<?php
+include_once '../lib/Database.php';
+include_once '../helpers/Format.php';
+
+class Category
+{
+    private $db;
+    private $format;
+
+    /**
+     * Category constructor.
+     */
+    public function __construct()
+    {
+        $this->db = new Database();
+        $this->format = new Format();
+    }
+
+    public function catInsert($catName)
+    {
+        $catName = $this->format->validation($catName);
+        $catName = mysqli_real_escape_string($this->db->link, $catName);
+
+        if (empty($catName)) {
+            $msg = "<span class='error'>Category field must not be empty!!</span>";
+
+            return $msg;
+        } else {
+            $query = "INSERT INTO tbl_category(catName) VALUES ('$catName')";
+            $catinsert = $this->db->insert($query);
+
+            if ($catinsert)
+            {
+                $msg = "<span class='success'>Category Inserted Successfully.</span>";
+                return $msg;
+            } else {
+                $msg = "<span class='error'>Category Not Inserted.</span>";
+                return $msg;
+            }
+        }
+    }
+
+    public function getAllCat()
+    {
+        $query = "SELECT * FROM tbl_category ORDER BY catId DESC";
+        $result = $this->db->select($query);
+
+        return $result;
+    }
+
+    public function getCatById($id)
+    {
+        $query = "SELECT * FROM tbl_category WHERE catId = '$id'";
+        $result = $this->db->select($query);
+
+        return $result;
+    }
+
+    public function catUpdate($catName, $id)
+    {
+        $catName = $this->format->validation($catName);
+        $id = mysqli_real_escape_string($this->db->link, $id);
+        $catName = mysqli_real_escape_string($this->db->link, $catName);
+
+        if (empty($catName)) {
+            $msg = "<span class='error'>Category field must not be empty!!</span>";
+
+            return $msg;
+        } else {
+            $query = "UPDATE tbl_category SET catName = '$catName' WHERE catId = '$id'";
+            $updated_row = $this->db->update($query);
+
+            if ($updated_row)
+            {
+                $msg = "<span class='success'>Category Updateed Successfully.</span>";
+                return $msg;
+            } else {
+                $msg = "<span class='error'>Category Not Updated.</span>";
+                return $msg;
+            }
+        }
+    }
+
+    public function delCatById($id)
+    {
+        $id = mysqli_real_escape_string($this->db->link, $id);
+        $query = "DELETE FROM tbl_category WHERE catId = '$id'";
+        $deldata = $this->db->delete($query);
+
+        if ($deldata)
+        {
+            $msg = "<span class='success'>Category Deleted Successfully.</span>";
+            return $msg;
+        } else {
+            $msg = "<span class='error'>Category Not Deleted.</span>";
+            return $msg;
+        }
+    }
+}
